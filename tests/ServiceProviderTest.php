60ebcf102793a400d84dd8ccf047977217e21075
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-04-28 00:42:02 -0400

    Removed unused imports.

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -3,16 +3,6 @@
 namespace Tests;
 
 use DarkGhostHunter\Laradate\LaradateServiceProvider;
-use DarkGhostHunter\Laralerts\Bag;
-use DarkGhostHunter\Laralerts\Facades\Alert;
-use DarkGhostHunter\Laralerts\Http\Middleware\StorePersistentAlertsInSession;
-use DarkGhostHunter\Laralerts\LaralertsServiceProvider;
-use DarkGhostHunter\Laralerts\View\Component\LaralertsComponent;
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Arr;
-use Illuminate\View\Compilers\BladeCompiler;
 use Orchestra\Testbench\TestCase;
 
 class ServiceProviderTest extends TestCase
